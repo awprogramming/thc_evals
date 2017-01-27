b6f080c8b35a7b17a21e62500301bbc28dce9401
Arie <awprogramming@gmail.com>
2017-01-27 13:10:28 -0500

    Minimal Mobile Optimization

--- dbconfig.php
+++ dbconfig.php
@@ -9,9 +9,14 @@
     public static function getInstance() {
       if (!isset(self::$instance)) {
         define('DB_USERNAME','root');
-		define('DB_PASSWORD','ariewolfdb');
-		define('DB_SERVER','localhost');
-		define('DB_DATABASE','ariedw5_evals');
+        define('DB_PASSWORD','ariewolfdb');
+        define('DB_SERVER','localhost');
+        define('DB_DATABASE','ariedw5_evals');
+
+        // define('DB_USERNAME','tylerhilleval');
+    		// define('DB_PASSWORD','EfhJLEVLeWHMVknkBNq7EuBc');
+    		// define('DB_SERVER','localhost');
+    		// define('DB_DATABASE','tylerhilleval');
 		$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die( "Unable to connect");
 		$database = mysqli_select_db($connection,DB_DATABASE) or die( "Unable to select database");
         self::$instance = $connection;
